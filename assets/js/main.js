4d4e220a6cd12061425db4acbca11ea7a063175b
Ujii-san615 <ujiie.c615@gmail.com>
2021-06-02 21:00:14 +0900

    scriptリンクを修正

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,21 @@
-$(function(){
+$(function () {
 	$('#pagetop').hide();
-		// スクロールした場合のアクションが記されています
-		$(window).scroll(function () {
-			if ($(this).scrollTop() > 200){
-				$('#pagetop').fadeIn();
-				// スクロール位置が200px以下の場合は「ページトップへ戻るボタン」を消しておく（フェードアウトで消える）
-			} else {
-				$('#pagetop').fadeOut();
-			}
-		})
-	});
+	// スクロールした場合のアクションが記されています
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('#pagetop').fadeIn();
+			// スクロール位置が200px以下の場合は「ページトップへ戻るボタン」を消しておく（フェードアウトで消える）
+		} else {
+			$('#pagetop').fadeOut();
+		}
+	})
+});
 
-	//「ページトップへ戻るボタン」をクリックした場合のページトップへ戻るスピードの速さが記されています
-	$('#pagetop').click(function(){
-		$('html,body').animate({
-			scrollTop: 0 }, 500);
-		return false;
-	});
+//「ページトップへ戻るボタン」をクリックした場合のページトップへ戻るスピードの速さが記されています
+$('#pagetop').click(function () {
+	$('html,body').animate({
+		scrollTop: 0
+	}, 1000);
+	return false;
+});
+
